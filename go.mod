31e53f59331ef8f83887e4c73e6c8bdc601bc29e
Gema Adhi <geadwi@gmail.com>
2021-03-29 08:39:03 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/odit-bit/timestamp
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("timestamp package")
+}
+
+func Timestamp() {
+	fmt.Println("this is Timestamp module function call")
+}
